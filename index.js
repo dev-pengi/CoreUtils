5a5f59ba1ee589dc47e8bccd5957b1531b76aeb7
dev-pengi <siffffdoul@gmail.com>
2023-03-31 15:37:19 +0100

    reimported the modules due to the new structure

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const time = require('./src/time');
 const format = require('./src/format');
-const utils = require('./src/utils');
 const canvas = require('./src/canvas');
+const arrays = require('./src/arrays');
+const math = require('./src/math');
+const objects = require('./src/objects');
+const random = require('./src/random');
 
 
 module.exports = {
     time,
     format,
-    utils,
     canvas,
+    arrays,
+    math,
+    objects,
+    random
 }
